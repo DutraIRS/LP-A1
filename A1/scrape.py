59ec2c7f44af4f6d5e5c6283d5877d106be3cda2
Gerardo Mikael Do Carmo Pereira <gerardomikael1234@gmail.com>
2022-10-02 01:08:52 -0300

    Comentários corrigidos e padronizados

--- A1/scrape.py
+++ A1/scrape.py
@@ -3,28 +3,26 @@ from bs4 import BeautifulSoup
 import requests
 
 def pegar_html(url: str):
-    '''
-	A função recebe um url do site lyrics.com, busca o html e retorna uma string 
+    '''A função recebe um url do site lyrics.com, busca o html e retorna uma string 
     referente a página dada pela url
 
 	:param url: Link da página da internet que se deseja buscar o html
 	:url type: str
 	:return: Texto do html do link dado
-	:r type: str
+	:rtype: str
 	'''
 
     texto_html = requests.get(url)
     return texto_html.text
 
 def pegar_letra(url: str):
-    '''
-	A função recebe um url do site lyrics.com, busca a letra da música que está presente
+    '''A função recebe um url do site lyrics.com, busca a letra da música que está presente
     nesse html e retorna uma string referente a letra dessa música
 
 	:param url: Link da página da música
 	:url type: str
 	:return: letra da música
-	:r type: str
+	:rtype: str
 	'''
 
     soup = BeautifulSoup(pegar_html(url), 'html.parser')
@@ -36,14 +34,14 @@ def pegar_letra(url: str):
         print('Letra não encontrada')
 
 def pegar_letras_musicas_album(url: str):
-    '''
-	A função recebe um url do site lyrics.com, busca as letras de todas as músicas
-    desse album e retorna uma lista com essas músicas
+    '''A função recebe um url do site lyrics.com referente ao álbum de uma banda,
+    busca as letras de todas as músicas desse album e retorna uma lista com essas 
+    músicas
 
-	:param url: Link da página do álbum escolhido
+	:param url: Link da página do álbum
 	:url type: str
 	:return: Letras das músicas do álbum
-	:r type: list
+	:rtype: list
 	'''
 
     soup = BeautifulSoup(pegar_html(url), 'html.parser')
@@ -72,14 +70,13 @@ def pegar_letras_musicas_album(url: str):
         return lista_letras
 
 def pegar_titulo(url: str):
-    '''
-	A função recebe um url do site lyrics.com, busca o nome da música que está
+    '''A função recebe um url do site lyrics.com, busca o nome da música que está
     presente nesse link o retorna
 
 	:param url: Link da página da música
 	:url type: str
 	:return: Título da música
-	:r type: str
+	:rtype: str
 	'''
 
     soup = BeautifulSoup(pegar_html(url), 'html.parser')
@@ -90,15 +87,15 @@ def pegar_titulo(url: str):
         print('erro')
 
 def pegar_titulos_musicas_album(url: str):
-    '''
-	A função recebe um url do site lyrics.com, busca os títulos de todas as músicas
-    desse album e retorna uma lista com esses títulos
+    '''A função recebe um url do site lyrics.com referente ao álbum de uma banda, busca 
+    os títulos de todas as músicas desse álbum e retorna uma lista com esses títulos
 
-	:param url: Link da página do álbum escolhido
+	:param url: Link da página do álbum
 	:url type: str
 	:return: Títulos das músicas do álbum
-	:r type: list
+	:rtype: list
 	'''
+
     soup = BeautifulSoup(pegar_html(url), 'html.parser')
     album = soup.findAll('table', attrs={'class': 'table tdata'})
 
@@ -107,7 +104,7 @@ def pegar_titulos_musicas_album(url: str):
     #semelhante à função 'pegar_letras_musicas_album' dentro do link do album buscaremos todos os links que levam as suas músicas
     for div in album:
         links = div.find_all('a')
-        #Ao pegar todos esses links usaremos a função 'pegar_titulo' para pegar a letra de cada uma das músicas
+        #Ao pegar todos esses links usaremos a função 'pegar_titulo' para pegar o título de cada uma das músicas
         for a in links:
             letra_album = "https://www.lyrics.com/" + a['href']
             print('procurando título no link: ', "https://www.lyrics.com/" + a['href'])
@@ -121,18 +118,19 @@ def pegar_titulos_musicas_album(url: str):
         return lista_titulos
 
 def pegar_ano(url: str):
-    '''
-	A função recebe um url do site lyrics.com, busca o ano de lançamento da música que está presente
-    nesse html e retorna uma string referente ao seu ano de lançamento
+    '''A função recebe um url do site lyrics.com, busca o ano de lançamento da música que está presente
+    nesse link e retorna uma string referente ao seu ano de lançamento
     
 	:param url: Link da página da música
 	:url type: str
 	:return: Ano de lançamento
-	:r type: str
+	:rtype: str
 	'''
+
     soup = BeautifulSoup(pegar_html(url), 'html.parser')
     tempo_ano = soup.findAll('dd', attrs={'class': 'dd-margin'})
 
+    #O ano de lançamento da música se encontra em uma lista, sendo ele o primeiro elemento
     tempo_ano_list = []
     for item in tempo_ano:
         tempo_ano_list.append(item)
@@ -147,15 +145,15 @@ def pegar_ano(url: str):
         return None
 
 def pegar_ano_musicas_album(url: str):
-    '''
-	A função recebe um url do site lyrics.com, busca o tempo de todas as músicas
-    desse álbum e retorna uma lista com esses tempos
+    '''A função recebe um url do site lyrics.com referente ao álbum de uma banda, busca
+    o tempo de todas as músicas desse álbum e retorna uma lista com esses tempos
 
-	:param url: Link da página da música
+	:param url: Link da página do álbum
 	:url type: str
 	:return: Tempo das músicas do álbum
-	:r type: list
+	:rtype: list
 	'''
+
     soup = BeautifulSoup(pegar_html(url), 'html.parser')
     album = soup.findAll('table', attrs={'class': 'table tdata'})
 
@@ -164,7 +162,7 @@ def pegar_ano_musicas_album(url: str):
     #dentro do link do album buscaremos todos os links que levam as suas músicas
     for div in album:
         links = div.find_all('a')
-        #Ao pegar todos esses links usaremos a função 'pegar_letra' para pegar a letra de cada uma das músicas
+        #Ao pegar todos esses links usaremos a função 'pegar_ano' para pegar o ano de lançamento de cada uma das músicas
         for a in links:
             tempo_album = "https://www.lyrics.com/" + a['href']
             print('procurando tempo no link: ', "https://www.lyrics.com/" + a['href'])
@@ -174,17 +172,16 @@ def pegar_ano_musicas_album(url: str):
 
     return lista_tempos
         
-
 def pegar_titulo_album(url: str):
-    '''
-	A função recebe um url do site lyrics.com, busca o tempo de todas as músicas
-    desse álbum e retorna uma lista com esses tempos
+    '''A função recebe um url do site lyrics.com referente ao album de uma banda, busca o título
+    desse álbum e o retorna
 
-	:param url: Link da página da música
+	:param url: Link da página do álbum
 	:url type: str
-	:return: Tempo das músicas do álbum
-	:r type: list
+	:return: Título do álbum
+	:rtype: str
 	'''
+
     soup = BeautifulSoup(pegar_html(url), 'html.parser')
     album = soup.find('h1')
 
@@ -192,6 +189,15 @@ def pegar_titulo_album(url: str):
     return titulo_album.replace(' Album', '')
 
 def pegar_compositores(url: str):
+    '''A função recebe um url do site lyrics.com, busca o(s) compositor(es) da música desse desse url
+    e retorna o(s) seu(s) nome(s)
+
+	:param url: Link da página da música
+	:url type: str
+	:return: Compositores da música
+	:rtype: str
+	'''
+
     soup = BeautifulSoup(pegar_html(url), 'html.parser')
     detalhes = soup.find('div', attrs={'class': 'lyric-credits'})
     
@@ -201,14 +207,13 @@ def pegar_compositores(url: str):
     return compositores.replace('Written by: ', '')
 
 def pegar_compositores_musicas_album(url: str):
-    '''
-	A função recebe um url do site lyrics.com, busca o tempo de todas as músicas
-    desse álbum e retorna uma lista com esses tempos
+    '''A função recebe um url do site lyrics.com referente ao álbum de uma banda, busca 
+    os compositores de todas as músicas desse álbum e retorna uma lista com esses compositores
 
-	:param url: Link da página da música
+	:param url: Link da página do álbum
 	:url type: str
-	:return: Tempo das músicas do álbum
-	:r type: list
+	:return: Compositores das músicas do álbum
+	:rtype: list
 	'''
     soup = BeautifulSoup(pegar_html(url), 'html.parser')
     album = soup.findAll('table', attrs={'class': 'table tdata'})
@@ -218,7 +223,7 @@ def pegar_compositores_musicas_album(url: str):
     #dentro do link do album buscaremos todos os links que levam as suas músicas
     for div in album:
         links = div.find_all('a')
-        #Ao pegar todos esses links usaremos a função 'pegar_letra' para pegar a letra de cada uma das músicas
+        #Ao pegar todos esses links usaremos a função 'pegar_compositores' para pegar os compositores de cada uma das músicas
         for a in links:
             compositores_album = "https://www.lyrics.com/" + a['href']
             print('procurando compositores no link: ', "https://www.lyrics.com" + a['href'])
@@ -228,14 +233,13 @@ def pegar_compositores_musicas_album(url: str):
     return lista_compositores
 
 def pegar_link_youtube(url: str):
-    '''
-	A função recebe um url do site lyrics.com, busca a letra da música que está presente
-    nesse html e retorna uma string referente a letra dessa música
+    '''A função recebe um url do site lyrics.com, busca o link do youtube da música
+    nesse link e retorna o link
 
 	:param url: Link da página da música
 	:url type: str
-	:return: letra da música
-	:r type: str
+	:return: Link da música no Youtube
+	:rtype: str
 	'''
 
     soup = BeautifulSoup(pegar_html(url), 'html.parser')
@@ -243,14 +247,13 @@ def pegar_link_youtube(url: str):
     return views
 
 def pegar_visualizacoes(url: str):
-    '''
-	A função recebe um url do site lyrics.com, busca a letra da música que está presente
-    nesse html e retorna uma string referente a letra dessa música
+    '''A função recebe um url do site youtube.com, busca o número de visualizações
+    neste link e o retorna
 
-	:param url: Link da página da música
+	:param url: Link do youtube de uma música
 	:url type: str
-	:return: letra da música
-	:r type: str
+	:return: Número de visualizações
+	:rtype: str
 	'''
 
     soup = BeautifulSoup(pegar_html(url), 'html.parser')
@@ -261,14 +264,13 @@ def pegar_visualizacoes(url: str):
         return None
 
 def pegar_visualizacoes_musicas_album(url:str):
-    '''
-	A função recebe um url do site lyrics.com, busca o tempo de todas as músicas
-    desse álbum e retorna uma lista com esses tempos
+    '''A função recebe um url do site lyrics.com, busca o número de visualizações
+    de todas as músicas desse álbum e retorna uma lista com esses números
 
-	:param url: Link da página da música
+	:param url: Link da página do álbum
 	:url type: str
-	:return: Tempo das músicas do álbum
-	:r type: list
+	:return: Lista com número de visualizações
+	:rtype: list
 	'''
     soup = BeautifulSoup(pegar_html(url), 'html.parser')
     album = soup.findAll('table', attrs={'class': 'table tdata'})
@@ -278,7 +280,8 @@ def pegar_visualizacoes_musicas_album(url:str):
     #dentro do link do album buscaremos todos os links que levam as suas músicas
     for div in album:
         links = div.find_all('a')
-        #Ao pegar todos esses links usaremos a função 'pegar_letra' para pegar a letra de cada uma das músicas
+        #Ao pegar todos esses links usaremos a função 'pegar_link_youtube' para obter o vídeo no youtube e 'pegar_visualizacoes' para 
+        #pegar as visualizacoes de cada uma das músicas
         for a in links:
             links_musicas_album = "https://www.lyrics.com/" + a['href']
             
@@ -287,7 +290,6 @@ def pegar_visualizacoes_musicas_album(url:str):
                 link = pegar_link_youtube(links_musicas_album)
                 print('Pegando link do youtube: ', link)
             
-
             visualizacoes = pegar_visualizacoes('http://youtube.com/watch?v='+link)
             print('Pegando views no link ', 'http://youtube.com/watch?v='+link)
             try:
@@ -297,37 +299,45 @@ def pegar_visualizacoes_musicas_album(url:str):
     return lista_visualizacoes
 
 def gerar_dataframe_album(url: str):
-    '''
-    Usando as funções criadas obteremos um dataframe de um álbum dado a partir de
-    um url do site lyrics.com
+    '''Usando as funções criadas está função cria um dataframe de um álbum dado a partir de
+    um url do site lyrics.com da página desse álbum
     
-	:param url: Link da página da música
+	:param url: Link da página do álbum
 	:url type: str
 	:return: Dataframe do álbum
-	:r type: DataFrame
+	:rtype: DataFrame
     '''
     print('O processo pode demorar um pouco dependendo da quantidade de músicas. Aguarde...')
-    dados = {'Ano':pegar_ano_musicas_album(url),'Compositores':pegar_compositores_musicas_album(url), 'letra':pegar_letras_musicas_album(url)}
+    #O conteúdo do dataframe é formado por ano, compositores, visualizações e letra, e seus índices são os títulos das músicas
+    dados = {
+        'Ano':pegar_ano_musicas_album(url),
+        'Compositores':pegar_compositores_musicas_album(url), 
+        'Visualizações':pegar_visualizacoes_musicas_album(url), 
+        'Letra':pegar_letras_musicas_album(url)}
+
     df = pd.DataFrame(data=dados, index=[pegar_titulos_musicas_album(url)])
     print(df)
     return df
 
 def gerar_dataframe_banda(url_list: list):
     '''
-    Usando as funções criadas e recebendo como parâmetro uma lista de links obteremos
-    um dataframe de uma banda dado a partir da lista de urls do site lyrics.com
+    Usando as funções criadas e recebendo como parâmetro uma lista de links esta
+    função obtem um dataframe de uma banda dada a partir de uma lista de urls com
+    os seus álbums
     
 	:param url_list: Lista de links dos albuns da banda
 	:url_list type: list
 	:return: Dataframe da banda
-	:r type: DataFrame
+	:rtype: DataFrame
     '''
+
     lista_titulos = []
     lista_letras = []
     lista_anos = []
     lista_compositores = []
     lista_titulo_album = []
     lista_visualizacoes = []
+
     for album in url_list:
         lista_titulos += pegar_titulos_musicas_album(album)
         lista_letras += pegar_letras_musicas_album(album)
